dde3d0f1c8a70505d1e41127a4fbe2c70b62a947
FIRST_NAME LAST_NAME <jobsonbrito10@gmail.com>
2021-09-07 23:23:25 -0300

    finalizando

--- src/index.js
+++ src/index.js
@@ -1,42 +1,52 @@
 const express = require("express");
 const axios = require("axios");
-const Acao = require("./models/acoes")
+const Acao = require("./models/acoes");
 
 const app = express();
 
 const port = 3333;
 
-
 Acao.sync();
 
-
 const getLatestPrice = async (symbol) => {
-    const response = await axios.get(`https://cloud.iexapis.com/stable/stock/${symbol}/quote?token=sk_b802f575af34471aab0a68307f414b7f`);
-    return response;
-}
-
+  const response = await axios.get(
+    `https://cloud.iexapis.com/stable/stock/${symbol}/quote?token=sk_b802f575af34471aab0a68307f414b7f`
+  );
+  return response;
+};
 
 
 app.get("/", (req, res) => {
-    return res.json({ hello: "World"});
-})
+  return res.json({ hello: "World" });
+});
 
+app.get("/price/:symbol", async (req, res) => {
+  const symbol = req.params.symbol;
+  symbol.toUpperCase();
 
-app.get("/price/:symbol", async (req,res) => {
-    const symbol = req.params.symbol;
-    symbol.toUpperCase();
-
-
+  const acao = await Acao.findOne({ where: { symbol: symbol } });
+  if (acao == null) {
     const quote = await getLatestPrice(symbol);
-
-    const acao = {
+    if (quote) {
+      const newAcao = {
         symbol: quote.data.symbol,
         name: quote.data.companyName,
-        latestPrice: quote.data.latestPrice
+        latestPrice: quote.data.latestPrice,
+      };
+      Acao.create(newAcao);
+      res.json(newAcao);
+      return;
     }
-    res.json(acao);
-})
+
+    res.json({ error: "Ação não encontrada!"})
+  }
+  res.json({
+    nameOfCompany: acao.name,
+    symbol: acao.symbol,
+    latestPrice: acao.latestPrice,
+  });
+});
 
 app.listen(port, () => {
-    console.info(`App rodando em http://localhost:${port}`);
-})
+  console.info(`App rodando em http://localhost:${port}`);
+});
